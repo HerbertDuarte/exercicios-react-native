93f044b49b44c4caf6cd3f8d655187a069456943
Herbert Duarte <herbertduarte.santos@gmail.com>
2023-10-29 16:55:29 -0300

    Update App.js

--- 01-Navigation/App.js
+++ 01-Navigation/App.js
@@ -1,6 +1,6 @@
 import Routes from './routes/Routes';
 import { StatusBar } from 'expo-status-bar';
-import { ThemeProvider } from '@react-navigation/native';
+import { NavigationContainer, ThemeProvider } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import SignIn from './screens/SignIn';
 
@@ -10,21 +10,23 @@ export default function App() {
   return (
     <ThemeProvider theme="light">
       <StatusBar style='dark' />
-      <Stack.Navigator 
-        initialRouteName='SignIn'
-        screenOptions={{
-          headerShown : false
-        }}
-        >
-        <Stack.Screen
-          name='SignIn'
-          component={SignIn}
-        />
-        <Stack.Screen
-          name='Routes'
-          component={Routes}
-        />
-      </Stack.Navigator>
+      <NavigationContainer>
+        <Stack.Navigator
+          initialRouteName='SignIn'
+          screenOptions={{
+            headerShown : false
+          }}
+          >
+          <Stack.Screen
+            name='SignIn'
+            component={SignIn}
+          />
+          <Stack.Screen
+            name='Routes'
+            component={Routes}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
     </ThemeProvider>
   );
 }
